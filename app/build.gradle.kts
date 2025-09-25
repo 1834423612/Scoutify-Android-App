d07e3c0937a2787374001ec58d07ae3bdceef210
kjch <1834423612@qq.com>
2025-09-24 23:43:20 -0400

    build: Update compileSdk and targetSdk to 35, add ProGuard

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,16 +6,17 @@ plugins {
 
 android {
     namespace = "com.team695.scoutifyapp"
-    compileSdk = 36
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.team695.scoutifyapp"
         minSdk = 24
-        targetSdk = 36
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        proguardFiles()
     }
 
     buildTypes {
