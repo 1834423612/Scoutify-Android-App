d8136ecf8d11fc609bbff5394395bc482f3376ad
kjch <1834423612@qq.com>
2025-09-24 23:48:56 -0400

    build: Update compileSdk and targetSdk to 36, and updated AndroidX Navigation Compose to 2.9.5 and Accompanist System UI Controller to 0.36.0.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,12 +6,12 @@ plugins {
 
 android {
     namespace = "com.team695.scoutifyapp"
-    compileSdk = 35
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.team695.scoutifyapp"
         minSdk = 24
-        targetSdk = 35
+        targetSdk = 36
         versionCode = 1
         versionName = "1.0"
 
@@ -50,8 +50,8 @@ dependencies {
     implementation(libs.androidx.compose.ui.graphics)
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
-    implementation("androidx.navigation:navigation-compose:2.8.3")
-    implementation("com.google.accompanist:accompanist-systemuicontroller:0.34.0") // 控制状态栏颜色
+    implementation("androidx.navigation:navigation-compose:2.9.5")
+    implementation("com.google.accompanist:accompanist-systemuicontroller:0.36.0") // 控制状态栏颜色
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
