4a4fccf7661a1f49d4599f0972aef4a6e0cd62d2
alexfinger21 <cdx9933@gmail.com>
2026-01-26 00:08:35 -0500

    viewmodel class

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -52,6 +52,8 @@ dependencies {
     implementation(libs.androidx.compose.material3)
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.6.4")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.6.1")
     implementation("androidx.navigation:navigation-compose:2.9.5")
     implementation("com.google.accompanist:accompanist-systemuicontroller:0.36.0") // 控制状态栏颜色
     testImplementation(libs.junit)
--- app/src/main/java/com/team695/scoutifyapp/ui/screens/home/HomeViewModel.kt
+++ app/src/main/java/com/team695/scoutifyapp/ui/screens/home/HomeViewModel.kt
@@ -4,13 +4,22 @@ import com.team695.scoutifyapp.data.api.model.Match
 import com.team695.scoutifyapp.data.api.service.MatchService
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
-import kotlinx.coroutines.viewModelScope
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.launch
 
-class HomeViewModel(private val service: MatchService) {
+class HomeViewModel(private val service: MatchService): ViewModel() {
     private val _matches = MutableStateFlow<List<Match>>(emptyList())
     val matches: StateFlow<List<Match>> = _matches
 
     fun fetchMatches() {
-
+        viewModelScope.launch {
+            try {
+                val matchesRes = service.listMatches()
+                _matches.value = matchesRes
+            } catch (e: Exception) {
+                println("Error fetching matches: ${e.message}")
+            }
+        }
     }
 }
