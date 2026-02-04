cfdecb7990a9e0048d5ba8da85861a39bb9cd033
igor023 <thedarthdot@gmail.com>
2026-02-04 07:53:36 -0500

    .

--- app/src/main/java/com/team695/scoutifyapp/ui/screens/tasks/TasksViewModel.kt
+++ app/src/main/java/com/team695/scoutifyapp/ui/screens/tasks/TasksViewModel.kt
@@ -9,7 +9,7 @@ import kotlinx.coroutines.flow.update
 class TaskRepository {
     fun getTasks(): List<Task> {
         return listOf(
-            Task(1, 2, "6935", "02m", 0.5f, false, "PARTIAL"),
+            Task(1, 2, "695", "02m", 0.5f, false, "PARTIAL"),
             Task(2, 3, "118", "01m", 0.25f, false, "PARTIAL"),
             Task(3, 4, "254", "03m", 1.0f, true, "COMPLETE"),
             Task(4, 5, "148", "02m", 0.0f, false, "INCOMPLETE"),
