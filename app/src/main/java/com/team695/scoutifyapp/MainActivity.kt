87d86c6b3aac0d42ca8d473512d04dca04807be5
alexfinger21 <cdx9933@gmail.com>
2026-02-06 18:00:23 -0500

    login screen

--- app/src/main/java/com/team695/scoutifyapp/MainActivity.kt
+++ app/src/main/java/com/team695/scoutifyapp/MainActivity.kt
@@ -8,15 +8,5 @@ import androidx.compose.material3.Surface
 import com.team695.scoutifyapp.ui.screens.*
 import com.team695.scoutifyapp.ui.theme.ScoutifyTheme
 
-class MainActivity : ComponentActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContent {
-            ScoutifyTheme {
-                Surface(color = MaterialTheme.colorScheme.background) {
-                    MainScreen()
-                }
-            }
-        }
-    }
+class MainActivity : LoginScreen() {
 }
