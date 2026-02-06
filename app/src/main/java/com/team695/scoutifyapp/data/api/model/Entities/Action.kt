6e263f446141a35d9ea3a3260989649fd1965a94
kjch <1834423612@qq.com>
2026-02-06 12:24:28 -0500

    fix: update Action class to include ActionTypeConverter and set default type

--- app/src/main/java/com/team695/scoutifyapp/data/api/model/Entities/Action.kt
+++ app/src/main/java/com/team695/scoutifyapp/data/api/model/Entities/Action.kt
@@ -1,13 +1,16 @@
 package com.team695.scoutifyapp.data.api.model.Entities
 
+import io.objectbox.annotation.Convert
 import io.objectbox.annotation.Entity
 import io.objectbox.annotation.Id
+import io.objectbox.converter.PropertyConverter
 import io.objectbox.relation.ToOne
 
 @Entity
 data class Action(
     @Id var id: Long = 0,
-    var type: ActionType,
+    @Convert(converter = ActionTypeConverter::class, dbType = Int::class)
+    var type: ActionType = ActionType.Defense,
     var timestamp: Long = 0,
 ) {
     lateinit var match: ToOne<Match>
@@ -17,4 +20,14 @@ enum class ActionType {
     Cycle,
     Stockpile,
     Brick
+}
+
+class ActionTypeConverter : PropertyConverter<ActionType, Int> {
+    override fun convertToDatabaseValue(entityProperty: ActionType): Int {
+        return entityProperty.ordinal
+    }
+
+    override fun convertToEntityProperty(databaseValue: Int): ActionType {
+        return ActionType.entries[databaseValue]
+    }
 }
