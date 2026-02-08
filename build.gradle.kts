17b8b7b159912ac20413b1b48b64b15a7f02f396
alexfinger21 <cdx9933@gmail.com>
2026-02-08 15:17:16 -0500

    removed objectbox

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,11 +11,7 @@
 //    alias(libs.plugins.kotlin.compose) apply false
 //    alias(libs.plugins.kotlin.android) apply false
 //}// Top-level build file where you can add configuration options common to all sub-projects/modules.
-buildscript {
-    dependencies{
-        classpath("io.objectbox:objectbox-gradle-plugin:4.0.2")
-    }
-}
+
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.compose.compiler) apply false
