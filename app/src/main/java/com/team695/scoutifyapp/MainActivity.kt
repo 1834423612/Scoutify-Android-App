d320df06994ef7eddbec3fd6e04399843d090aa8
alexfinger21 <cdx9933@gmail.com>
2026-02-09 13:50:43 -0500

    homescreen service

--- app/src/main/java/com/team695/scoutifyapp/MainActivity.kt
+++ app/src/main/java/com/team695/scoutifyapp/MainActivity.kt
@@ -3,6 +3,8 @@ package com.team695.scoutifyapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import com.team695.scoutifyapp.data.api.ScoutifyClient
+import com.team695.scoutifyapp.data.api.service.MatchService
 import com.team695.scoutifyapp.ui.theme.ScoutifyTheme
 import com.team695.scoutifyapp.ui.viewModels.TaskService
 
@@ -10,11 +12,15 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        val taskService: TaskService = TaskService()
+        val taskService = TaskService()
+        val matchService: MatchService = ScoutifyClient.matchService
 
         setContent {
             ScoutifyTheme {
-                Root(taskService = taskService)
+                Root(
+                    taskService = taskService,
+                    matchService = matchService
+                    )
             }
         }
     }
