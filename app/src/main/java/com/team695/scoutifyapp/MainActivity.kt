0a504d198513e3a8a4dc7321514bbbd1948a9092
alexfinger21 <cdx9933@gmail.com>
2026-02-09 12:33:40 -0500

    changed to dependency injection to reduce type unsafety

--- app/src/main/java/com/team695/scoutifyapp/MainActivity.kt
+++ app/src/main/java/com/team695/scoutifyapp/MainActivity.kt
@@ -4,14 +4,17 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import com.team695.scoutifyapp.ui.theme.ScoutifyTheme
+import com.team695.scoutifyapp.ui.viewModels.TaskService
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        val taskService: TaskService = TaskService()
+
         setContent {
             ScoutifyTheme {
-                Root()
+                Root(taskService = taskService)
             }
         }
     }
