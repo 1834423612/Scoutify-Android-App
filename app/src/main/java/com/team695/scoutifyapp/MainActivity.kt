979af39c3b60149b65819b0862727647cc5ab22f
alexfinger21 <61606770+alexfinger21@users.noreply.github.com>
2026-02-09 14:06:13 -0500

    Features master (#12)
    * viewmodels update
    
    * changed to dependency injection to reduce type unsafety
    
    * removed old comments and fixed type check
    
    * new home state
    
    * home state bug fixes
    
    * homescreen service
    # Conflicts:
    #	app/src/main/java/com/team695/scoutifyapp/MainActivity.kt

--- app/src/main/java/com/team695/scoutifyapp/MainActivity.kt
+++ app/src/main/java/com/team695/scoutifyapp/MainActivity.kt
@@ -20,7 +20,7 @@ class MainActivity : ComponentActivity() {
                 Root(
                     taskService = taskService,
                     matchService = matchService
-                )
+                    )
             }
         }
     }
