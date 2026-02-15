1f8eb4a9b6848aad68ea0a7afa3953702d8ff465
SuperMeerkat345 <artiom22903@gmail.com>
2026-02-15 13:28:04 -0500

    added NetworkService and its respective implementation

---
+++ app/src/main/java/com/team695/scoutifyapp/data/api/NetworkMonitor.kt
@@ -0,0 +1,51 @@
+package com.team695.scoutifyapp.data.api
+
+import android.content.Context
+import android.net.ConnectivityManager
+import android.net.Network
+import android.net.NetworkCapabilities
+import android.net.NetworkRequest
+import com.team695.scoutifyapp.data.api.service.NetworkService
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+
+
+// IMPLEMENTATION OF NetworkService
+class NetworkMonitor(private val context: Context) : NetworkService {
+    private val connectivityManager = context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+
+    private val _isConnected = MutableStateFlow(false)
+    override val isConnected: StateFlow<Boolean> = _isConnected
+
+    // listener for when network has changed
+    private val networkCallback = object : ConnectivityManager.NetworkCallback() {
+        // internet validated
+        override fun onCapabilitiesChanged(
+            network: Network,
+            capabilities: NetworkCapabilities
+        ) {
+            val validated = capabilities.hasCapability(NetworkCapabilities.NET_CAPABILITY_VALIDATED)
+            _isConnected.value = validated
+        }
+
+        // lost connection to network
+        override fun onLost(network: Network) {
+            _isConnected.value = false
+        }
+    }
+
+
+    override fun startMonitoring() {
+        val networkRequest = NetworkRequest.Builder()
+            .addCapability(NetworkCapabilities.NET_CAPABILITY_INTERNET)
+            .build()
+
+        // register the listener
+        connectivityManager.requestNetwork(networkRequest, networkCallback)
+    }
+
+    override fun stopMonitoring() {
+        // unregister the listener
+        connectivityManager.unregisterNetworkCallback(networkCallback)
+    }
+}
