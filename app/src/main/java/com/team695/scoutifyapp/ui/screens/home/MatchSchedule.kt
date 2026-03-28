202cc8de84a0b391f78e036afb8f4bab4ace2fa3
igor024 <thedarthdot@gmail.com>
2026-03-28 10:55:56 -0400

    switch bg img to wolstein

--- app/src/main/java/com/team695/scoutifyapp/ui/screens/home/MatchSchedule.kt
+++ app/src/main/java/com/team695/scoutifyapp/ui/screens/home/MatchSchedule.kt
@@ -14,6 +14,7 @@ import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
@@ -134,13 +135,14 @@ fun MatchSchedule(
                     .height(if (!teamState.isNullOrEmpty()) 150.dp else 100.dp) // Changed back to a fixed height
             ) {
                 Image(
-                    painter = painterResource(id = R.drawable.wolstein),
+                    painter = painterResource(id = R.drawable.cincinatti),
                     contentDescription = "Regional background",
                     contentScale = ContentScale.Crop,
                     modifier = Modifier
                         .fillMaxSize() // Safe to use fillMaxSize again with fixed height
                         .alpha(0.3f)
                         .clip(RoundedCornerShape(smallCornerRadius))
+                        .offset(y = 32.dp)
                 )
 
                 Column(
