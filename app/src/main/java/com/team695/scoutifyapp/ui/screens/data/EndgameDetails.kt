cb34a0a2ee283bf62a0213eff094d36367af7c39
igor024 <thedarthdot@gmail.com>
2026-03-29 08:56:25 -0400

    disable postgame button when cachedtime > 0

--- app/src/main/java/com/team695/scoutifyapp/ui/screens/data/EndgameDetails.kt
+++ app/src/main/java/com/team695/scoutifyapp/ui/screens/data/EndgameDetails.kt
@@ -64,7 +64,9 @@ fun EndgameDetails(
     )
     val previousTimer = formState.teleopCachedMilliseconds - currentTimer
     val coroutineScope = rememberCoroutineScope()
-    val endgameCompleted = formState.gameDetails.endgameProgress == 1f && formState.gameDetails.teleopCompleted == true
+    val endgameCompleted = formState.gameDetails.endgameProgress == 1f &&
+            formState.gameDetails.teleopCompleted == true &&
+            formState.teleopCachedMilliseconds == 0
 
     val timers = listOf(
         Timer(
