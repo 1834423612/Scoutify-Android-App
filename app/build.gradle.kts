be342a975e1a531bb8083ce5c3dd70162c9bf879
kjch <1834423612@qq.com>
2026-04-04 12:14:33 -0400

    chore(upgrade): Bump versionName from 1.0.3 to 1.4.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -28,7 +28,7 @@ configure<ApplicationExtension> {
         minSdk = 34
         targetSdk = 36
         versionCode = 1
-        versionName = "1.0.3"
+        versionName = "1.4.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
