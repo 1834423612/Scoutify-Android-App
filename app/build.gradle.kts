49159597e11940ca8dd95925d2ded3b354dac2fa
kjch <1834423612@qq.com>
2026-04-06 13:04:54 -0400

    chore: update versionCode to 42 for release 1.4.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -27,7 +27,7 @@ configure<ApplicationExtension> {
         applicationId = "com.team695.scoutifyapp"
         minSdk = 34
         targetSdk = 36
-        versionCode = 2
+        versionCode = 42
         versionName = "1.4.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
